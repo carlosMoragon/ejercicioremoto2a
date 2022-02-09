3690a53c6e956eb75a8a651c0e905acbb26afdd8
Carlos Moragon <cmoragoncorella@gmail.com>
2022-02-09 15:19:09 +0100

    añadido bucle

--- ejercicio2/src/aplicacion/Principal.java
+++ ejercicio2/src/aplicacion/Principal.java
@@ -5,17 +5,38 @@ import java.util.Scanner;
 public class Principal{
 	public static void main(String[] args){
 		Scanner sc = new Scanner(System.in);
+		boolean repetir = true;
+		while(repetir){
+			System.out.println("-----------------------------");
+			String suma = sc.nextLine();
 
-		String suma = sc.nextLine();
-
-		if(suma.contains("suma")){
-			String[] numero = suma.split(" ");
-			suma(Integer.parseInt(numero[1]));
-		}else if(suma.contains("!")){
-			String[] numero = suma.split("!");
-			factorial(Long.parseLong(numero[0]));
+			if(suma.contains("suma")){
+				String[] numero = suma.split(" ");
+				suma(Integer.parseInt(numero[1]));
+			}else if(suma.contains("!")){
+				String[] numero = suma.split("!");
+				factorial(Long.parseLong(numero[0]));
+			}else if(suma.equalsIgnoreCase("help")){
+				printHelp();
+			}else if(suma.equalsIgnoreCase("exit")){
+				repetir = false;
+			}else{
+				printHelp();
+				repetir = true;
+			}
 		}
+		sc.close();
 	}
+	
+	public static void printHelp(){
+		String mensaje = "Tienes estas opciones: \n" + 
+			"Ayuda: help\n" +
+			"Salir: exit\n" +
+			"Sumatorio: 'suma' (numero)\n" +
+			"Factorial: (numero)!\n";
+		System.out.println(mensaje);
+	}
+
 
 	public static void suma(int n){
 		long resultado = 0;
