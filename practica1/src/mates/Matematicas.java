83ee3a2aee6c1db0bef5981baacfd10cb1d3d97e
Carlos Moragon <cmoragoncorella@gmail.com>
2022-02-09 10:17:53 +0100

    añadido practica1

--- practica1/src/mates/Matematicas.java
+++ practica1/src/mates/Matematicas.java
@@ -1,13 +1,22 @@
 package mates;
 
-public class Matematicas{
-	/**
-	 * Genera una aproximació n al nú mero pi mediante el mé todo de
-	 * Montecarlo. El pará metro ` pasos` indica el nú mero de puntos
-	 * generado.
-	 */
-	public static double generarNumeroPiIterativo(long pasos){
+import java.lang.Math;
 
-		return 0;
+public class Matematicas{
+	public static double generarNumeroPiIterativo(long pasos){
+		double d = 0.00;
+		double x;
+		double y;
+		for(long i = 1; i <= pasos; i++){
+			x = Math.random();
+			y = Math.random();
+			if((Math.pow(x,2) + Math.pow(y,2)) <= 1){
+				d++;
+			}
+		}
+
+		double pi = (4*d)/(double) pasos;
+
+		return pi;
 	}
 }
