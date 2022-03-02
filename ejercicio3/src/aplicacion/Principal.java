dbaa7a8d620130293372c1364208e0c5a78fece3
Carlos Moragon <cmoragoncorella@gmail.com>
2022-03-02 09:00:28 +0100

    se intentan las torres

--- ejercicio3/src/aplicacion/Principal.java
+++ ejercicio3/src/aplicacion/Principal.java
@@ -1,23 +1,28 @@
 package aplicacion;
 public class Principal{
-
-	private static final String torreOrigen = "Inicio";
+	
+	/*private static final String torreOrigen = "Inicio";
 	private static final String torreAuxiliar = "Medio";
-	private static final String torreDestino = "Final";
+	private static final String torreDestino = "Final";*/
 
 	public static void main(String[] args){
-		torresDeHanoi(Integer.parseInt(args[0]), Integer.parseInt(args[0]), 0, 0);
+		torresDeHanoi(Integer.parseInt(args[0]), "inicio", "medio", "final");
 
 	}
 
-	public static void torresDeHanoi(long numeroDiscos, int origen, int auxiliar, int destino){
+	public static void torresDeHanoi(long numeroDiscos, String torreOrigen, String torreAuxiliar, String torreDestino){
 		if(numeroDiscos == 1){
 			System.out.println(torreOrigen + " a " + torreDestino);
 		}else{
+			torresDeHanoi(numeroDiscos - 1,torreOrigen, torreDestino, torreAuxiliar);
 			System.out.println(torreOrigen + " a " + torreAuxiliar);
-			torresDeHanoi(numeroDiscos - 1, origen - 1, auxiliar + 1, destino);
+			torresDeHanoi(numeroDiscos - 1, torreAuxiliar, torreOrigen, torreDestino);
+			//System.out.println(torreAuxiliar + " a " + torreDestino);
+			//torresDeHanoi(numeroDiscos - 1);
 		}
 
 	}
+
+	//Hay que darle valores a los discos
 
 }
