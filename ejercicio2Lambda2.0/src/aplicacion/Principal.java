3cc715dc163b99baf0b9569e8b8ecc69bcf8cfff
Carlos Moragon <cmoragoncorella@gmail.com>
2022-03-09 15:10:31 +0100

    clase 9/3/22

--- ejercicio2Lambda2.0/src/aplicacion/Principal.java
+++ ejercicio2Lambda2.0/src/aplicacion/Principal.java
@@ -1,14 +1,12 @@
 package aplicacion;
+
 import java.util.Arrays;
 import java.util.List;
-import java.util.ArrayList;
 import java.util.function.Supplier;
-import java.util.function.Function;
-import java.util.function.Consumer;
-import java.util.stream.DoubleStream;
+import java.util.stream.IntStream;
+
 public class Principal{
 	public static void main(String[] args){
-		Consumer<Object> print = System.out::println;
 		//Supplier<List<Double>> crea un objeto llamado lista, en el cual se encuentra la coleccion
 		Supplier<List<Double>> listaDouble = () -> Arrays.asList(8.9, 4.23, 6.32);
 		
@@ -26,14 +24,10 @@ public class Principal{
 	}
 	
 	public static void sumatorio(/*Consumer<Object> print, */List<Integer> lista){
-		int solucion = lista.stream().reduce(0, (x, y) -> x + y);
+		int solucion = lista.stream().reduce(0, Integer::sum);
 		System.out.println("Sumatorio: " + solucion);
 		//print.apply(solucion);
 	}
-
-	/*public static void sumatorioD(List<Double> lista){
-		double solucion = DoubleStream(lista).reduce(0, (x,y) -> x + y);
-	}*/
 
 	public static void productorio(/*Consumer<Object> print, */List<Integer> lista){
 		int solucion = lista.stream().reduce(1, (x,y) -> x * y);
@@ -43,15 +37,9 @@ public class Principal{
 	}
 
 	public static void factorial(int n){
-		List<Integer> lista = new Arraylist<>();
-		Function<Integer, Integer> f = x -> {
-			for(int i = 1; i<= x; i++){
-				lista.add(i);
-			}
-		};
-
-		
-		f.apply(n);
-		lista.get().forEach(System.out::println);
-	}	
+		int solucion = IntStream.range(1, n).reduce(1, (x, y) -> x * y);
+		System.out.println("9!: " + solucion);
+	}
+	
+	public static void media(
 }
