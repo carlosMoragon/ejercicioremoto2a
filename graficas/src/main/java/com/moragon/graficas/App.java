cf0c20d56a88f64571d454f0c24a13dd04bb7875
Carlos Moragon <cmoragoncorella@gmail.com>
2022-04-27 10:03:07 +0200

    terminado ordenación, falta busqueda.

--- graficas/src/main/java/com/moragon/graficas/App.java
+++ graficas/src/main/java/com/moragon/graficas/App.java
@@ -26,11 +26,18 @@ public class App {
         //dataset.addSeries("Busqueda Binaria", new double[][] {{ 10, 100, 1000, 10000,
 
         //Pruebas aleatorio
-        dataset.addSeries("burbuja", new double[][]{{10, 100, 1000, 10000/*, 100000*/}, {5100, 341800, 2908900, 37671000/*, 3818753100.00*/}});
-        dataset.addSeries("MergeSort", new double[][]{{10, 100, 1000, 10000/*, 100000*/}, {8900, 166700, 1350100, 136244000/*, 5846967000.00*/}});
-        dataset.addSeries("Seleccion Directa", new double[][]{{10, 100, 1000, 10000/*, 100000*/}, {4200, 244300, 8973200, 40758100/*, 2694775700.00*/}});
-        dataset.addSeries("Insercion Directa", new double[][]{{10, 100, 1000, 10000/*, 100000*/}, {4000, 245200, 4040300, 35845300/*, 2446415100.00*/}});
-        dataset.addSeries("QuickSort", new double[][]{{10, 100, 1000, 10000}, {4800, 294300, 5016100, 29275300}});
+        //dataset.addSeries("burbuja", new double[][]{{10, 100, 1000, 10000/*, 100000*/}, {16500,311100,10353000,243246300/*, 3818753100.00*/}});
+        //dataset.addSeries("MergeSort", new double[][]{{10, 100, 1000, 10000/*, 100000*/}, {24800,198300,5590000,92820700/*, 5846967000.00*/}});
+        //dataset.addSeries("Seleccion Directa", new double[][]{{10, 100, 1000, 10000/*, 100000*/}, {13400,498400,9939100,29921600/*, 2694775700.00*/}});
+        //dataset.addSeries("Insercion Directa", new double[][]{{10, 100, 1000, 10000/*, 100000*/}, {9400,108500,5305900,28665000/*, 2446415100.00*/}});
+        //dataset.addSeries("QuickSort", new double[][]{{10, 100, 1000, 10000}, {15700,76600,303200,1338400}});
+
+        //pruebas ordenado
+        dataset.addSeries("burbuja", new double[][]{{10, 100, 1000, 10000/*, 100000*/}, {4500,192300,5048300,29436700/*, 3818753100.00*/}});
+        dataset.addSeries("MergeSort", new double[][]{{10, 100, 1000, 10000/*, 100000*/}, {14600,138800,7368000,138080800/*, 5846967000.00*/}});
+        dataset.addSeries("Seleccion Directa", new double[][]{{10, 100, 1000, 10000/*, 100000*/}, {4400,425000,3737100,29888100/*, 2694775700.00*/}});
+        dataset.addSeries("Insercion Directa", new double[][]{{10, 100, 1000, 10000/*, 100000*/}, {2000,7800,13400,22800/*, 2446415100.00*/}});
+        dataset.addSeries("QuickSort", new double[][]{{10, 100, 1000, 10000}, {4700,249800,2730300,29109200}});
 
         XYLineAndShapeRenderer renderer = new XYLineAndShapeRenderer();
         renderer.setSeriesPaint(0, Color.ORANGE);
@@ -43,11 +50,11 @@ public class App {
         renderer.setSeriesStroke(2, new BasicStroke(2));
         renderer.setSeriesStroke(3, new BasicStroke(2));
         renderer.setSeriesStroke(4, new BasicStroke(2));
-        JFreeChart chart = ChartFactory.createXYLineChart("Pruebas Inverso", "Tamaño", "Tiempo", dataset);
+        JFreeChart chart = ChartFactory.createXYLineChart("Pruebas ordenado", "Tamaño", "Tiempo", dataset);
         chart.getXYPlot().getRangeAxis();
         ((NumberAxis) chart.getXYPlot().getRangeAxis()).setNumberFormatOverride(new DecimalFormat(""));
         chart.getXYPlot().setRenderer(renderer);
         BufferedImage image = chart.createBufferedImage(600, 400);
-        ImageIO.write(image, "png", new File("Pruebas-inverso.png"));
+        ImageIO.write(image, "png", new File("Pruebas-ordenado.png"));
     }
 }
