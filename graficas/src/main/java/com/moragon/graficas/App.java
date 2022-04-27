719f77746d1c0ee539e7fcffac89149e8fc28c53
Carlos Moragon <cmoragoncorella@gmail.com>
2022-04-27 10:32:51 +0200

    terminado las graficas

--- graficas/src/main/java/com/moragon/graficas/App.java
+++ graficas/src/main/java/com/moragon/graficas/App.java
@@ -22,8 +22,8 @@ public class App {
         //dataset.addSeries("Seleccion Directa", new double[][]{{10, 100, 1000, 10000/*, 100000*/}, {4200, 244300, 8973200, 40758100/*, 2694775700.00*/}});
         //dataset.addSeries("Insercion Directa", new double[][]{{10, 100, 1000, 10000/*, 100000*/}, {4000, 245200, 4040300, 35845300/*, 2446415100.00*/}});
         //dataset.addSeries("QuickSort", new double[][]{{10, 100, 1000, 10000}, {4800, 294300, 5016100, 29275300}});
-        dataset.addSeries("busqueda Lineal", new double[][] {{10, 100, 1000, 10000, 100000 }, {82200,16600,146000,417600,1555000}});
-        dataset.addSeries("Busqueda Binaria", new double[][] {{ 10, 100, 1000, 10000},{}};
+        //dataset.addSeries("busqueda Lineal", new double[][] {{10, 100, 1000, 10000, 100000 }, {82200,16600,146000,417600,1555000}});
+        //dataset.addSeries("Busqueda Binaria", new double[][] {{ 10, 100, 1000, 10000,100000},{10900,3300,3400,3800,5000}});
 
         //Pruebas aleatorio
         //dataset.addSeries("burbuja", new double[][]{{10, 100, 1000, 10000/*, 100000*/}, {16500,311100,10353000,243246300/*, 3818753100.00*/}});
@@ -31,8 +31,8 @@ public class App {
         //dataset.addSeries("Seleccion Directa", new double[][]{{10, 100, 1000, 10000/*, 100000*/}, {13400,498400,9939100,29921600/*, 2694775700.00*/}});
         //dataset.addSeries("Insercion Directa", new double[][]{{10, 100, 1000, 10000/*, 100000*/}, {9400,108500,5305900,28665000/*, 2446415100.00*/}});
         //dataset.addSeries("QuickSort", new double[][]{{10, 100, 1000, 10000}, {15700,76600,303200,1338400}});
-        dataset.addSeries("busqueda Lineal", new double[][] {{10, 100, 1000, 10000, 100000 }, {82200,16600,146000,417600,1555000}});
-        dataset.addSeries("Busqueda Binaria", new double[][] {{ 10, 100, 1000, 10000},{}};
+        //dataset.addSeries("busqueda Lineal", new double[][] {{10, 100, 1000, 10000, 100000 }, {1900,6500,57700,536600,667000}});
+        //dataset.addSeries("Busqueda Binaria", new double[][] {{ 10, 100, 1000, 10000,100000},{2400,2400,2300,2800,2500}});
 
         //pruebas ordenado
         //dataset.addSeries("burbuja", new double[][]{{10, 100, 1000, 10000/*, 100000*/}, {4500,192300,5048300,29436700/*, 3818753100.00*/}});
@@ -40,8 +40,8 @@ public class App {
         //dataset.addSeries("Seleccion Directa", new double[][]{{10, 100, 1000, 10000/*, 100000*/}, {4400,425000,3737100,29888100/*, 2694775700.00*/}});
         //dataset.addSeries("Insercion Directa", new double[][]{{10, 100, 1000, 10000/*, 100000*/}, {2000,7800,13400,22800/*, 2446415100.00*/}});
         //dataset.addSeries("QuickSort", new double[][]{{10, 100, 1000, 10000}, {4700,249800,2730300,29109200}});
-        dataset.addSeries("busqueda Lineal", new double[][] {{10, 100, 1000, 10000, 100000 }, {82200,16600,146000,417600,1555000}});
-        dataset.addSeries("Busqueda Binaria", new double[][] {{ 10, 100, 1000, 10000},{}};
+        //dataset.addSeries("busqueda Lineal", new double[][] {{10, 100, 1000, 10000, 100000 }, {3200,6700,55800,528900,673000}});
+        //dataset.addSeries("Busqueda Binaria", new double[][] {{ 10, 100, 1000, 10000,100000},{2800,2700,3000,2400,6800}});
 
         XYLineAndShapeRenderer renderer = new XYLineAndShapeRenderer();
         renderer.setSeriesPaint(0, Color.ORANGE);
